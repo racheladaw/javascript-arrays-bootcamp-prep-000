1c26188de099699607169dc1e21b6fa193acb2c2
Rachel Williams <williamsrachelada@gmail.com>
2019-01-21 23:52:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -38,3 +38,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
   return array
 }
+function removeElementFromEndOfArray(array) {
+  array = array.slice(0, array.length - 1)
+  return array
+}
