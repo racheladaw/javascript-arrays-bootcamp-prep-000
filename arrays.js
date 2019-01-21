2df8c59fe53ad53c6301f00ae59bba5dabff4e61
Rachel Williams <williamsrachelada@gmail.com>
2019-01-21 22:53:11 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-// function addElementToBeginningOfArray(array, 'element') {
-//   array = ['element', ...array]
-//   return array 
-// }
+function addElementToBeginningOfArray(array, 'element') {
+  array = ['element', ...array]
+  return array 
+}
